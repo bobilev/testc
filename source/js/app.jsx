2101e8ce01b0357e5fe473f790cc69f47de63026
bobilev <l7upoy@gmail.com>
2019-09-21 23:11:28 +0300

    f

--- source/js/app.jsx
+++ source/js/app.jsx
@@ -2,14 +2,16 @@ import { hot } from 'react-hot-loader/root';
 import '@vkontakte/vkui/dist/vkui.css';
 import React, { Component } from "react";
 import {View, Panel, PanelHeader, Group, List, Cell, ConfigProvider, Root, HeaderButton,platform, IOS} from '@vkontakte/vkui';
-import vkConnect from '@vkontakte/vk-connect';
+import connect from '@vkontakte/vk-connect';
+import qrvk from '@vkontakte/vk-qr'
 import CellButton from "@vkontakte/vkui/dist/components/CellButton/CellButton";
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
 // import '../styles/app.css';
 
-vkConnect.send("VKWebAppInit", {})
+
 
 class App extends Component {
     constructor(props) {
@@ -17,11 +19,49 @@ class App extends Component {
         this.state = {
             activeView: 'view1',
             activePanel: "panel1",
-            history: ['main']
+            history: ['main'],
+            QRcode : "",
+            QRcodeRes: ""
         }
     }
+    componentDidMount() {
+        connect.send("VKWebAppInit", {})
 
+        connect.subscribe((e) => {
+            console.log("e",e)
+            switch (e.detail.type) {
+                case 'VKWebAppGetUserInfoResult':
+                    break;
+                case 'VKWebAppAccessTokenReceived':
+                    break;
+                case 'VKWebAppGetPhoneNumber':
+                    break;
+                case 'VKWebAppOpenQRResult':
+                    console.log("QR",e.detail)
+                    this.setState(() => {
+                        return {QRcodeRes: e.detail.data.qr_data}
+                    })
+                    break;
+                default:
+                    console.log(e.detail.type);
+            }
+        });
+        connect.send('VKWebAppGetUserInfo', {});
+        // connect.send("VKWebAppGetPhoneNumber", {});
+
+        const text = 'https://vk.com/evgbobilev';
+        const isShowVkLogo = true;
+        const qrSvg = qrvk.createQR(text, 256, 'qr-code-class', isShowVkLogo);
+        this.setState(() => {
+            return {QRcode:qrSvg}
+        })
+        // connect.send('VKWebAppGetAuthToken', {'app_id': 6906999, 'scope': 'market'});
+    }
+    onOpenQRcamera = () => {
+        connect.send("VKWebAppOpenQR");
+    }
     render() {
+        let { QRcode,QRcodeRes } = this.state
         const osname = platform();
         return (
             <Root activeView={this.state.activeView}>
@@ -32,6 +72,13 @@ class App extends Component {
                             <CellButton onClick={ () => this.setState({ activeView: 'view2' }) }>
                                 Open View 2
                             </CellButton>
+                            <div className="qr_code" dangerouslySetInnerHTML={{ __html: QRcode }}></div>
+                            <Button onClick={this.onOpenQRcamera}>
+                                Сканировать QR-код
+                            </Button>
+                            <div>
+                                QRcodeRes:{QRcodeRes}
+                            </div>
                         </Group>
                     </Panel>
                 </View>
